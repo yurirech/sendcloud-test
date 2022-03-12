c8e5a2efcfffd89172fa97c117b8fadeaba0a9bb
Yuri Rech <yuri.nunes@tuntscorp.com>
2022-03-12 17:35:27 +0100

    finished second part

--- src/index.js
+++ src/index.js
@@ -4,18 +4,10 @@ import "./styles/main.scss";
 // document.getElementById('button1').onclick = activateButton
 
 let navbarButtons = document.querySelectorAll(".navbar-button");
-navbarButtons.forEach((button) =>
+navbarButtons.forEach((button) => {
 	button.addEventListener("click", (e) => {
+		let activeButton = document.querySelector(".navbar-button.active");
+		activeButton.classList.remove("active");
 		button.classList.add("active");
-		deactivateOtherButtons(button.id);
-	})
-);
-
-function deactivateOtherButtons(id) {
-	navbarButtons.forEach((button) =>
-  {
-    if(button.id !== id) {
-      button.classList.remove('active')
-  }}
-  )
-}
+	});
+});
